bc7b1a521c30f67a1b69089d6729cd07fbcdeada
Josh <joshmerces07@gmail.com>
2026-02-23 14:11:29 -0300

    responsive changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,32 +5,22 @@ import './App.css'
 import Contact from "./pages/contact/contact.tsx";
 import Links from "./pages/links/links.tsx";
 import Work from "./pages/works/Work.tsx";
-import {useEffect} from "react";
 
 
 function App() {
-        useEffect(() => {
-            const baseWidth = window.innerWidth;
-
-            const applyZoom = () => {
-                const zoom = window.innerWidth / baseWidth;
-                document.body.style.zoom = String(zoom);
-            };
-
-            window.addEventListener("resize", applyZoom);
-            return () => window.removeEventListener("resize", applyZoom);
-        }, []);
     return (
-      <div className="app">
-          <Routes>
-              <Route path='/' element={<Home/>}/>
-              <Route path='/about' element={<About/>}/>
-              <Route path='/contact' element={<Contact/>}/>
-              <Route path='/links' element={<Links/>}/>
-              <Route path='/work' element={<Work/>}/>
-          </Routes>
-      </div>
-  )
+        <div className="app">
+            <div className="pageContainer">
+                <Routes>
+                    <Route path='/' element={<Home/>}/>
+                    <Route path='/about' element={<About/>}/>
+                    <Route path='/contact' element={<Contact/>}/>
+                    <Route path='/links' element={<Links/>}/>
+                    <Route path='/work' element={<Work/>}/>
+                </Routes>
+            </div>
+        </div>
+    )
 }
 
 export default App;
